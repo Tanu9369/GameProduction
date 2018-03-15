b994e161a5c90f09e9638c39a877d0448815cca2
Tanawat Brian Huntakul <tbh3@njit.edu>
2018-03-14 22:33:26 -0400

    Added Placeholder Art and Scripts
    Placehold art are simple geometry
    Scripts
    -Player Movement WASD
    -Enemy Movement chase the player

---
+++ NotTheHero/Assets/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * speed * Time.deltaTime);
+		transform.Translate (Vector3.up * Input.GetAxis ("Vertical") * speed * Time.deltaTime);
+	}
+}
