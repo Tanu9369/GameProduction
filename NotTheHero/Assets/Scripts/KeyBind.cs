9c46559c94a5c39e4b3dcbab1b8419ff9ea188d4
Tanu9369 <tbh3@njit.edu>
2018-05-09 01:00:33 -0400

    my soul is dying

--- NotTheHero/Assets/Scripts/KeyBind.cs
+++ NotTheHero/Assets/Scripts/KeyBind.cs
@@ -1,16 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class KeyBind : MonoBehaviour {
+public class KeyBind : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    public Text up, left, down, right;
+    private GameObject currentKey;
+    private Color32 normal = new Color32(255, 255, 255, 255);
+    private Color32 selected = new Color32(239, 116, 36, 255);
+
+
+    // Use this for initialization
+    void Start()
+    {
+        keys.Add("Up",  (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "KeyPad8")));
+        keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "KeyPad5")));
+        keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "KeyPad4")));
+        keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "KeyPad6")));
+
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keys["Up"]))
+        {
+            Debug.Log("Up");
+        }
+        if (Input.GetKeyDown(keys["Down"]))
+        {
+            Debug.Log("Down");
+        }
+        if (Input.GetKeyDown(keys["Left"]))
+        {
+            Debug.Log("Left");
+        }
+        if (Input.GetKeyDown(keys["Right"]))
+        {
+            Debug.Log("Right");
+        }
+    }
+
+    void OnGUI()
+    {
+        if (currentKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = normal;
+                currentKey = null;
+            }
+        }
+    }
+    public void ChangeKey(GameObject clicked)
+    {
+        if (currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = normal;
+        }
+        currentKey = clicked;
+        currentKey.GetComponent<Image>().color = selected;
+    }
+
+    public void SaveKeys()
+    {
+        foreach (var key in keys)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+        PlayerPrefs.Save();
+    }
 }
---
+++ NotTheHero/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+
+    public static AudioClip bgmSound;
+    static AudioSource audioSrc;
+	// Use this for initialization
+	void Start () {
+        bgmSound = Resources.Load<AudioClip>("bgm");
+
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+
+        }
+    }
+}
